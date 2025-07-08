733e684719f6a0b6034bde29e470c431bcca9f57
BrandoAp <avilabrando14@gmail.com>
2025-07-07 19:59:09 -0500

    feat: dependencia de tailwindcss agregado

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js,php}",
+    "./src/**/*.{html,js,php}",
+    "./assets/**/*.{html,js,php}",
+    "./components/**/*.{html,js,php}",
+    "./pages/**/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
