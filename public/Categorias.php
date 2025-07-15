9d34822c2767b2f64034684e8823d43c45717d60
shZzzz <131565081+Gerxld@users.noreply.github.com>
2025-07-15 08:42:17 -0500

    Comienzo a trabajar en CRUD CATEGORIAS

---
+++ public/Categorias.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New System - Categorias</title>
+    <link rel="stylesheet" href="../src/output.css">
+</head>
+<body class="bg-gray-100 w-full flex justify-center">
+    <div class="flex flex-col items-center p-10 bg-gray-50 rounded-3xl w-6xl m-5 shadow-lg">
+        <h1 class="text-3xl text-blue-900 font-bold mb-2">Categorias</h1>
+        <span class=""></span>
+    </div>
+</body>
+</html>
