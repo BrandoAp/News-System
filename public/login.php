40cc60c610c5b346ccf74362f0010e7a006eccd6
BrandoAp <avilabrando14@gmail.com>
2025-07-21 08:55:47 -0500

    fix: login.php add new styles

--- public/login.php
+++ public/login.php
@@ -10,7 +10,7 @@ unset($_SESSION['error']);
 <html lang="es">
 <head>
   <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <title>Login</title>
   <link rel="stylesheet" href="../public/css/login.css">
 </head>
